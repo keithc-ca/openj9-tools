9928cef6cebab33cc71078db4ff693f16a47d179
Keith W. Campbell <keithc@ca.ibm.com>
2020-08-03 11:11:27 -0400

    Don't pull bare repos
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- pull.sh
+++ pull.sh
@@ -5,8 +5,11 @@ repos=$(sed -e 's|#.*$||' < $here/repos)
 
 for repo in $repos ; do
 	if [ -e $here/$repo/.git/config ] ; then
-		echo ""
-		echo "Repository: $repo"
-		git -C $here/$repo pull
+		bare="$(git -C $here/$repo config core.bare 2>/dev/null || echo default)"
+		if [ $bare != true ] ; then
+			echo ""
+			echo "Repository: $repo"
+			git -C $here/$repo pull --ff-only
+		fi
 	fi
 done
