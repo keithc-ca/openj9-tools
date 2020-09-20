5c70d002f7e7239a705568b0004293551d46313d
Keith W. Campbell <keithc@ca.ibm.com>
2020-09-20 13:29:47 -0400

    Add support for setting up a RHEL system
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- system-setup.sh
+++ system-setup.sh
@@ -27,49 +27,109 @@ if [ $(id -u) != 0 ] ; then
 	fatal "This script requires root privileges."
 fi
 
-apt-get update
+apt_setup() {
+	apt-get update
 
-apt-get install -qq -y --no-install-recommends \
-	software-properties-common
+	apt-get install -qq -y --no-install-recommends \
+		software-properties-common
 
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
+	add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
-apt-get update
+	apt-get update
 
-apt-get install -qq -y --no-install-recommends \
-	autoconf \
-	ca-certificates \
-	ccache \
-	cmake \
-	cpio \
-	file \
-	g++-7 \
-	g++-8 \
-	git \
-	git-core \
-	less \
-	libasound2-dev \
-	libcups2-dev \
-	libdwarf-dev \
-	libelf-dev \
-	libfontconfig1-dev \
-	libfreetype6-dev \
-	$libnuma \
-	libssl-dev \
-	libx11-dev \
-	libxext-dev \
-	libxrandr-dev \
-	libxrender-dev \
-	libxt-dev \
-	libxtst-dev \
-	make \
-	$nasm \
-	pkg-config \
-	ssh \
-	systemtap-sdt-dev \
-	unzip \
-	vim \
-	wget \
-	zip
+	apt-get install -qq -y --no-install-recommends \
+		autoconf \
+		ca-certificates \
+		ccache \
+		cmake \
+		cpio \
+		file \
+		g++-7 \
+		g++-8 \
+		git \
+		git-core \
+		less \
+		libasound2-dev \
+		libcups2-dev \
+		libdwarf-dev \
+		libelf-dev \
+		libfontconfig1-dev \
+		libfreetype6-dev \
+		$libnuma \
+		libssl-dev \
+		libx11-dev \
+		libxext-dev \
+		libxrandr-dev \
+		libxrender-dev \
+		libxt-dev \
+		libxtst-dev \
+		make \
+		$nasm \
+		pkg-config \
+		ssh \
+		systemtap-sdt-dev \
+		unzip \
+		vim \
+		wget \
+		zip
 
-rm -rf /var/lib/apt/lists/*
+	rm -rf /var/lib/apt/lists/*
+}
+
+yum_setup() {
+	yum -y update
+
+	yum -y install \
+		alsa-lib-devel \
+		autoconf \
+		automake \
+		bind-utils \
+		bzip2 \
+		ca-certificates \
+		cpio \
+		cups-devel \
+		curl-devel \
+		elfutils-libelf-devel \
+		expat-devel \
+		file-devel \
+		file-libs \
+		fontconfig-devel \
+		freetype-devel \
+		gettext-devel \
+		glibc-devel \
+		gmp-devel \
+		libdwarf-devel \
+		libffi-devel \
+		libstdc++-static \
+		libX11-devel \
+		libXext-devel \
+		libXi-devel \
+		libXrandr-devel \
+		libXrender-devel \
+		libXt-devel \
+		libXtst-devel \
+		make \
+		mesa-libGL-devel \
+		mpfr-devel \
+		$nasm \
+		numactl-devel \
+		openssh-clients \
+		openssh-server \
+		openssl-devel \
+		systemtap-devel \
+		texinfo \
+		unzip \
+		wget \
+		xorg-x11-server-Xvfb \
+		xz \
+		zip \
+		zlib-devel
+}
+
+if type apt-get > /dev/null 2>&1 ; then
+	apt_setup
+elif type yum > /dev/null 2>&1 ; then
+	yum_setup
+else
+	fatal "Unsupported distribution"
+fi
