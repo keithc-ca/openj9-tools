459d421957307e5d808f370b9d96668ef3fdc043
Keith W. Campbell <keithc@ca.ibm.com>
2021-01-27 14:03:25 -0500

    Improve support for multiple build configurations
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- images-all.sh
+++ images-all.sh
@@ -1,25 +1,30 @@
 #!/bin/bash -
 
+# we want an empty list if there's no match
+shopt -s nullglob
+
 here=$(dirname $0)
-repos=$(sed -n -e 's|#.*$||' -e '/jdk/p' < $here/repos)
+repos=$(sed -n -e 's|#.*$||' -e 's|\t| |g' -e 's|^  *||' -e '/^jdk/p' < $here/repos)
 
 for repo in $repos ; do
 	if [ -e $here/$repo/.git/config ] ; then
 		echo ""
 		echo "Repository: $repo"
 
-		spec=$(echo $here/$repo/build/*/spec.gmk)
-		if [ -f "$spec" ] ; then
-			# Look for this line in $spec:
+		specs=0
+		for spec in $here/$repo/build/*/spec.gmk ; do
+			# Look for this line in spec.gmk:
 			# using 'configure ...'
 			configuration=$(sed -n -e "s|# using '\(configure [^']*\)'|\1|p" < $spec)
-		else
-			configuration=
-		fi
-		if [ -n "$configuration" ] ; then
-			make -C $here/$repo images
-		else
-			echo "No existing configuration found for $repo"
+			if [ -z "$configuration" ] ; then
+				echo "No configuration found in $spec"
+			else
+				( cd $(dirname $spec) && echo "Building $PWD . . ." && make images )
+			fi
+			specs=1
+		done
+		if [ $specs -eq 0 ] ; then
+			echo "No build configurations found for $repo"
 		fi
 	fi
 done
--- reconfigure-all.sh
+++ reconfigure-all.sh
@@ -1,25 +1,31 @@
 #!/bin/bash -
 
+# we want an empty list if there's no match
+shopt -s nullglob
+
 here=$(dirname $0)
-repos=$(sed -n -e 's|#.*$||' -e '/jdk/p' < $here/repos)
+repos=$(sed -n -e 's|#.*$||' -e 's|\t| |g' -e 's|^  *||' -e '/^jdk/p' < $here/repos)
 
 for repo in $repos ; do
 	if [ -e $here/$repo/.git/config ] ; then
 		echo ""
 		echo "Repository: $repo"
 
-		spec=$(echo $here/$repo/build/*/spec.gmk)
-		if [ -f "$spec" ] ; then
-			# Look for this line in $spec:
+		specs=0
+		for spec in $here/$repo/build/*/spec.gmk ; do
+			# Look for this line in spec.gmk:
 			# using 'configure ...'
 			configuration=$(sed -n -e "s|# using '\(configure [^']*\)'|\1|p" < $spec)
-		else
-			configuration=
-		fi
-		if [ -n "$configuration" ] ; then
-			( cd $here/$repo && bash $configuration )
-		else
-			echo "No existing configuration found for $repo"
+			if [ -z "$configuration" ] ; then
+				echo "No configuration found in $spec"
+			else
+				specname=$(basename $(dirname $spec))
+				( cd $here/$repo && echo "Reconfiguring $PWD/build/$specname . . ." && bash $configuration )
+			fi
+			specs=1
+		done
+		if [ $specs -eq 0 ] ; then
+			echo "No build configurations found for $repo"
 		fi
 	fi
 done
