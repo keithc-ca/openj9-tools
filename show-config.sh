fc0ebed41e57bc98e3b7f82defafc1b442a79110
Keith W. Campbell <keithc@ca.ibm.com>
2026-06-18 13:32:54 -0400

    Enhance show-config.sh to accept a conf-name pattern
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- show-config.sh
+++ show-config.sh
@@ -4,22 +4,26 @@ show_spec() {
   sed -n -e 's/^ *CONFIGURE_COMMAND_LINE *:= *//p'
 }
 
-specs=( build/*/spec.gmk )
-
-if [ ${#specs[@]} -gt 1 -o -f "$specs" ] ; then
-  :
-elif [ -f spec.gmk ] ; then
-  specs=( spec.gmk )
+if [ -f spec.gmk ] ; then
+  show_spec < spec.gmk
 else
-  echo "No existing configuration found"
   specs=( )
-fi
-
-if [ ${#specs[@]} -eq 1 ] ; then
-  show_spec < "${specs[@]}"
-else
-  for spec in "${specs[@]}" ; do
-    printf "%s\n  " "$spec:"
-    show_spec < "$spec"
+  pattern="${1:-.}"
+  for config in $(cd build 2>/dev/null && echo *) ; do
+    spec="build/$config/spec.gmk"
+    if [ -f "$spec" ] && echo "$config" | grep -q "$pattern" ; then
+      specs+=( "$spec" )
+    fi
   done
+
+  if [ ${#specs[@]} -eq 0 ] ; then
+    echo "No matching configurations found."
+  elif [ ${#specs[@]} -eq 1 ] ; then
+    show_spec < "${specs[@]}"
+  else
+    for spec in "${specs[@]}" ; do
+      printf "%s\n  " "$spec:"
+      show_spec < "$spec"
+    done
+  fi
 fi
